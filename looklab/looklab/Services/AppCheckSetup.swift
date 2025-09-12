0543629f9bd677b714bf1f7a35b42bcfdbef1b44
utkusen <utku@utkusen.com>
2025-09-12 12:50:28 +0100

    clean

---
+++ looklab/looklab/Services/AppCheckSetup.swift
@@ -0,0 +1,15 @@
+import Foundation
+#if canImport(FirebaseAppCheck)
+import FirebaseAppCheck
+import FirebaseCore
+
+final class LookLabAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
+    func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
+        if #available(iOS 14.0, *) {
+            return AppAttestProvider(app: app)
+        } else {
+            return DeviceCheckProvider(app: app)
+        }
+    }
+}
+#endif
